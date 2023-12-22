b12100647e8e8fe083e41e706f41a47218369852
thatchung <134276054+Khanhtq90@users.noreply.github.com>
2023-12-22 12:55:42 +0700

    update code

--- web/source/nuxt.config.js
+++ web/source/nuxt.config.js
@@ -189,7 +189,6 @@ module.exports = {
   },
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
     '@nuxtjs/i18n',
     '@nuxtjs/axios',
     '@nuxtjs/sitemap',
